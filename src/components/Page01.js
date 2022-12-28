2a6f1e5f7d83a8f2bf5118c88678ac31c4fc6f8b
Qa160601 <luonyeuvo2001@gmail.com>
2022-12-28 20:31:04 +0700

    done

--- src/components/Page01.js
+++ src/components/Page01.js
@@ -1,12 +1,52 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
 export default class Page01 extends Component {
+  constructor() {
+    super();
+    this.light = React.createRef();
+    this.pos = React.createRef();
+    this.mousemoveHandler = (e) => {
+      var x = e.pageX;
+      var y = e.pageY;
+
+      this.light.current.style.setProperty(
+        "--h",
+        this.pos.current.offsetHeight + "px"
+      );
+      this.light.current.style.setProperty("--x", x + "px");
+      this.light.current.style.setProperty("--y", y + "px");
+    };
+
+    this.Load = () => {
+      this.light.current.style.setProperty(
+        "--h",
+        this.pos.current.offsetHeight + "px"
+      );
+      this.light.current.style.setProperty(
+        "--h",
+        this.pos.current.offsetHeight + "px"
+      );
+      this.light.current.style.setProperty("--x", 50 + "%");
+      this.light.current.style.setProperty("--y", 35 + "%");
+    };
+    window.addEventListener("load", (event) => {
+      this.Load();
+    });
+  }
+
   render() {
+    // var light = document.querySelector(".grid");
+    // var pos = document.querySelector(".light");
+
     return (
       <>
         <div id="page1" />
-        <div id="particles-js" className="grid" />
-        <div className="light" />
+        <div id="particles-js" className="grid" ref={this.light} />
+        <div
+          className="light"
+          ref={this.pos}
+          onMouseMove={this.mousemoveHandler}
+        />
         <div className="content">
           <div className="main_content">
             <h1>CRUCIO</h1>
